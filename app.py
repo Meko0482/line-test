eef25b8172cffc3224db89d03332d2e6e427e9b0
Meko0482 <meko0482@gmail.com>
2024-06-09 16:53:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ def callback():
 
 # 氣象局 API 的 URL
 def fetch_weather_data(city):
+    # 氣象局 API 的 URL
     url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7A752AE1-2953-4680-A2BA-6B1B13AAB708&format=JSON&StationId=466900"
 
     try:
